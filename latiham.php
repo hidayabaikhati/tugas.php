246624253b8e2b96ea60d88925378432a764bf44
hidayabaikhati <109188713+hidayabaikhati@users.noreply.github.com>
2022-08-10 15:23:33 +0700

    Add files via upload

---
+++ latiham.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ 
+<P>tampilan perulanganb bilangan ganjil dari 100 - 0</P>
+<?php
+$i = 99;
+while($i <= 1){
+    echo "$i";
+    echo "<br/>";
+    $i-=2;
+}
+?>
+</body>
+</html>
